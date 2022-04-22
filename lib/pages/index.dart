93d6dfcd28b020fe499958fb69c3d8d343145f0c
baumlaufer0815 <developer.baumlaeufer0815@gmail.com>
2022-04-22 22:22:57 +0200

    changing background of icon to dark green

--- lib/pages/index.dart
+++ lib/pages/index.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:hausaufgaben/pages/homework/page.dart';
 
 import '../components/firebase/get_data.dart';
 import '../components/localStorage.dart';
+import '../styling.dart';
 
 class IndexPage extends StatelessWidget {
   final Future _storageFuture = localStorage.init();
@@ -15,9 +17,26 @@ class IndexPage extends StatelessWidget {
       future: Future.wait([GetData(), _storageFuture]),
       builder: (BuildContext context, AsyncSnapshot<List<dynamic>> snapshot) {
         if( snapshot.connectionState == ConnectionState.waiting){
-          return const Scaffold(
-            body: Center(
-                child: Text('Please wait its loading...')
+          return Scaffold(
+            appBar: AppBar(
+              backgroundColor: AppTheme.greyDarkGreen,
+              systemOverlayStyle: const SystemUiOverlayStyle(
+                  statusBarColor: AppTheme.greyDarkGreen
+              ),
+              title: Text(
+                'Laden',
+                style: AppTheme.textTheme.headline6,
+              ),
+            ),
+            body: const Center(
+              child: Text(
+                'Bitte warten, Daten werden abgerufen.',
+                overflow: TextOverflow.ellipsis,
+                style: TextStyle(
+                  fontSize: 18,
+                  fontFamily: 'Inter'
+                ),
+              ),
             ),
           );
         }else{
