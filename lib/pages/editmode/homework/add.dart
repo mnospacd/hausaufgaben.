c3f35da2ceed05f347068bed117121371d13852c
baumlaufer0815 <developer.baumlaeufer0815@gmail.com>
2022-04-24 21:43:47 +0200

    add "not add to history" option in edit mode

--- lib/pages/editmode/homework/add.dart
+++ lib/pages/editmode/homework/add.dart
@@ -5,11 +5,20 @@ import 'package:uuid/uuid.dart';
 
 import '../../../styling.dart';
 
-class EditHomeworkAdd extends StatelessWidget {
+class EditHomeworkAdd extends StatefulWidget {
   final TextEditingController subjectController = TextEditingController();
   final TextEditingController contentController = TextEditingController();
   final TextEditingController givenController = TextEditingController();
+
   EditHomeworkAdd({Key? key}) : super(key: key);
+
+  @override
+  State<EditHomeworkAdd> createState() => _EditHomeworkAddState();
+}
+
+class _EditHomeworkAddState extends State<EditHomeworkAdd> {
+  //add to history
+  bool? _noHistory = false;
 
   @override
   Widget build(BuildContext context) {
@@ -21,97 +30,36 @@ class EditHomeworkAdd extends StatelessWidget {
     String homeworkUuid = uuid.v4();
 
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: AppTheme.greyDarkGreen,
-        systemOverlayStyle: const SystemUiOverlayStyle(
-        statusBarColor: AppTheme.greyDarkGreen
-        ),
-        title: Text(
-          'Hinzufüge Hausaufgabe',
-          style: AppTheme.textTheme.headline6,
-        ),
-      ),
-      body: ListView(
-        padding: EdgeInsets.all(16),
-        children: [
-          TextField(
-            controller: subjectController,
-            maxLines: 1,
-            keyboardType: TextInputType.text,
-            autocorrect: false,
-            cursorColor: AppTheme.greyGreen,
-            style: const TextStyle(
-                fontFamily: 'Inter'
-            ),
-            decoration: const InputDecoration(
-              labelText: "Fach",
-              labelStyle: TextStyle(
-                  color: AppTheme.greyGreen,
-                  fontFamily: 'Inter'
-              ),
-              helperMaxLines: 3,
-              helperText: "Bitte den Kurznamen für das Fach benutzen, zB. Mathematik => Mathe.",
-              helperStyle: TextStyle(
-                  fontFamily: 'Inter'
-              ),
-              enabledBorder: UnderlineInputBorder(
-                borderSide: BorderSide(color: AppTheme.greyGreen),
-              ),
-              focusedBorder: UnderlineInputBorder(
-                borderSide: BorderSide(color: AppTheme.greyGreen),
-              ),
-            ),
+        appBar: AppBar(
+          backgroundColor: AppTheme.greyDarkGreen,
+          systemOverlayStyle: const SystemUiOverlayStyle(
+              statusBarColor: AppTheme.greyDarkGreen
           ),
-          Padding(
-            padding: const EdgeInsets.only(top: 8),
-            child: TextField(
-              controller: contentController,
-              maxLines: null,
-              keyboardType: TextInputType.multiline,
+          title: Text(
+            'Hinzufüge Hausaufgabe',
+            style: AppTheme.textTheme.headline6,
+          ),
+        ),
+        body: ListView(
+          padding: EdgeInsets.all(16),
+          children: [
+            TextField(
+              controller: widget.subjectController,
+              maxLines: 1,
+              keyboardType: TextInputType.text,
               autocorrect: false,
               cursorColor: AppTheme.greyGreen,
               style: const TextStyle(
                   fontFamily: 'Inter'
               ),
               decoration: const InputDecoration(
-                labelText: "Inhalt",
+                labelText: "Fach",
                 labelStyle: TextStyle(
                     color: AppTheme.greyGreen,
                     fontFamily: 'Inter'
                 ),
                 helperMaxLines: 3,
-                helperText: "Den Inhalt bitte nicht zu lange machen, am besten einfach direkt von der Tafel abschreiben.",
-                helperStyle: TextStyle(
-                  fontFamily: 'Inter'
-                ),
-                enabledBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(color: AppTheme.greyGreen),
-                ),
-                focusedBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(color: AppTheme.greyGreen),
-                ),
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 8),
-            child: TextField(
-              controller: givenController,
-              maxLines: null,
-              keyboardType: TextInputType.multiline,
-              autocorrect: false,
-              cursorColor: AppTheme.greyGreen,
-              style: const TextStyle(
-                  fontFamily: 'Inter'
-              ),
-              decoration: const InputDecoration(
-                labelText: "Aufgegeben",
-                labelStyle: TextStyle(
-                    color: AppTheme.greyGreen,
-                    fontFamily: 'Inter'
-                ),
-                helperMaxLines: 3,
-                helperText: "Hier kommt das Datum wann es aufgegeben wurde. Es folgt nach diesem Beispiel: Mo, 21. August (Kurztag, Nummer. Monat).",
+                helperText: "Bitte den Kurznamen für das Fach benutzen, zB. Mathematik => Mathe.",
                 helperStyle: TextStyle(
                     fontFamily: 'Inter'
                 ),
@@ -123,49 +71,125 @@ class EditHomeworkAdd extends StatelessWidget {
                 ),
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 12),
-            child: ListTile(
-              title: Text(
-                  homeworkUuid,
-                  style: AppTheme.textTheme.bodyText1?.copyWith(fontSize: 16)
-              ),
-              subtitle: const Text(
-                'Das ist eine UUID um Hausaufgaben zu identifizieren.',
-                style: TextStyle(fontFamily: 'Inter'),
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 12),
-            child: ElevatedButton(
-              onPressed: () {
-                ScaffoldMessenger.of(context).showSnackBar(
-                  const SnackBar(
-                    content: Text(
-                        'Hausaufgabe wird hochgeladen. Um sie zu sehen, musst du die App neustarten.',
-                      style: TextStyle(
-                        fontFamily: 'Inter'
-                      ),
-                    ),
-                  )
-                );
-                homework.doc(homeworkUuid).set({'Content': contentController.text.trim(), 'Given': givenController.text.trim(), 'Id': homeworkUuid, 'Subject': subjectController.text.trim()});
-                history.doc(homeworkUuid).set({'Content': contentController.text.trim(), 'Given': givenController.text.trim(), 'Subject': subjectController.text.trim()});
-                Navigator.pop(context);
-              },
-              style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all(AppTheme.greyGreen),
-                  textStyle: MaterialStateProperty.all(const TextStyle(
+            Padding(
+              padding: const EdgeInsets.only(top: 8),
+              child: TextField(
+                controller: widget.contentController,
+                maxLines: null,
+                keyboardType: TextInputType.multiline,
+                autocorrect: false,
+                cursorColor: AppTheme.greyGreen,
+                style: const TextStyle(
+                    fontFamily: 'Inter'
+                ),
+                decoration: const InputDecoration(
+                  labelText: "Inhalt",
+                  labelStyle: TextStyle(
+                      color: AppTheme.greyGreen,
                       fontFamily: 'Inter'
-                  ))
+                  ),
+                  helperMaxLines: 3,
+                  helperText: "Den Inhalt bitte nicht zu lange machen, am besten einfach direkt von der Tafel abschreiben.",
+                  helperStyle: TextStyle(
+                      fontFamily: 'Inter'
+                  ),
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: AppTheme.greyGreen),
+                  ),
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: AppTheme.greyGreen),
+                  ),
+                ),
               ),
-              child: const Text("Fertig"),
             ),
-          ),
-        ],
-      )
+            Padding(
+              padding: const EdgeInsets.only(top: 8),
+              child: TextField(
+                controller: widget.givenController,
+                maxLines: null,
+                keyboardType: TextInputType.multiline,
+                autocorrect: false,
+                cursorColor: AppTheme.greyGreen,
+                style: const TextStyle(
+                    fontFamily: 'Inter'
+                ),
+                decoration: const InputDecoration(
+                  labelText: "Aufgegeben",
+                  labelStyle: TextStyle(
+                      color: AppTheme.greyGreen,
+                      fontFamily: 'Inter'
+                  ),
+                  helperMaxLines: 3,
+                  helperText: "Hier kommt das Datum wann es aufgegeben wurde. Es folgt nach diesem Beispiel: Mo, 21. August (Kurztag, Nummer. Monat).",
+                  helperStyle: TextStyle(
+                      fontFamily: 'Inter'
+                  ),
+                  enabledBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: AppTheme.greyGreen),
+                  ),
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(color: AppTheme.greyGreen),
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 12),
+              child: CheckboxListTile(
+                title: Text(
+                    'Hinzufüge Hausaufgabe nicht zur Historie',
+                    style: AppTheme.textTheme.bodyText1?.copyWith(fontSize: 16)
+                ),
+                value: _noHistory,
+                onChanged: (bool? newValue) {
+                  setState(() {
+                    _noHistory = newValue;
+                  });
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 12),
+              child: ListTile(
+                title: Text(
+                    homeworkUuid,
+                    style: AppTheme.textTheme.bodyText1?.copyWith(fontSize: 16)
+                ),
+                subtitle: const Text(
+                  'Das ist eine UUID um Hausaufgaben zu identifizieren.',
+                  style: TextStyle(fontFamily: 'Inter'),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 12),
+              child: ElevatedButton(
+                onPressed: () {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(
+                        content: Text(
+                          'Hausaufgabe wird hochgeladen. Um sie zu sehen, musst du die App neustarten.',
+                          style: TextStyle(
+                              fontFamily: 'Inter'
+                          ),
+                        ),
+                      )
+                  );
+                  homework.doc(homeworkUuid).set({'Content': widget.contentController.text.trim(), 'Given': widget.givenController.text.trim(), 'Id': homeworkUuid, 'Subject': widget.subjectController.text.trim()});
+                  if (_noHistory == false) { history.doc(homeworkUuid).set({'Content': widget.contentController.text.trim(), 'Given': widget.givenController.text.trim(), 'Subject': widget.subjectController.text.trim()}); }
+                  Navigator.pop(context);
+                },
+                style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all(AppTheme.greyGreen),
+                    textStyle: MaterialStateProperty.all(const TextStyle(
+                        fontFamily: 'Inter'
+                    ))
+                ),
+                child: const Text("Fertig"),
+              ),
+            ),
+          ],
+        )
     );
   }
 }
